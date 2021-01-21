87bc63cd54a0fd5b09767489d1a9b8feaa4bc859
Shaun <Shaunsun@mumuxili.com>
2021-01-21 10:34:58 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <div class="btn">Open</div>
+    <div class="btn">Close</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class Home extends Vue {}
+</script>
+<style lang="less" scoped>
+.btn {
+  display: inline-block;
+  width: 100px;
+  padding: 10px 0;
+  margin: 0 16px;
+  text-align: center;
+  border: 1px solid #cccccc;
+  border-radius: 4px;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+const Path = require("path")
+module.exports = {
+  alias: {
+    "@": Path.resolve(__dirname, "../src")
+  }
+}
